9578c862d0d39d4147235a4810679d00802c5c69
Julian Baumann <git@julba.de>
2023-02-07 13:00:23 +0100

    Refactor

--- ffi/swift/build.sh
+++ ffi/swift/build.sh
@@ -17,8 +17,8 @@ IS_MAC=1 ARCHS="arm64" ./xc-universal-binary.sh data_rct ../data_rct_ffi $Config
 LLVM_TARGET_TRIPLE_SUFFIX="-simulator" ARCHS="arm64" ./xc-universal-binary.sh data_rct ../data_rct_ffi $Configuration
 LLVM_TARGET_TRIPLE_SUFFIX="-simulator" ARCHS="x86_64" ./xc-universal-binary.sh data_rct ../data_rct_ffi $Configuration
 
-rm -rf Output
-mkdir Output
+rm -rf .out
+mkdir .out
 
 echo ""
 echo "Generating dynamic macOS library"
@@ -27,7 +27,7 @@ echo "Generating dynamic macOS library"
 lipo -create \
   ../../target/x86_64-apple-darwin/release/libdata_rct.a \
   ../../target/aarch64-apple-darwin/release/libdata_rct.a \
-  -output Output/libdatarct_macos.a
+  -output .out/libdatarct_macos.a
 
 echo "Done."
 
@@ -37,7 +37,7 @@ echo "Generating dynamic iOS library"
 # Generate dynamic iOS library
 lipo -create \
   ../../target/aarch64-apple-ios/release/libdata_rct.a \
-  -output Output/libdatarct_ios.a
+  -output .out/libdatarct_ios.a
 
 echo "Done."
 
@@ -48,7 +48,7 @@ echo "Generating dynamic iOS simulator library"
 lipo -create \
   ../../target/x86_64-apple-ios/release/libdata_rct.a \
   ../../target/aarch64-apple-ios-sim/release/libdata_rct.a \
-  -output Output/libdatarct_ios_simulator.a
+  -output .out/libdatarct_ios_simulator.a
 
 echo "Done."
 
@@ -56,19 +56,16 @@ echo ""
 echo "Generating xcframework"
 
 xcodebuild -create-xcframework \
-  -library ./Output/libdatarct_macos.a \
+  -library ./.out/libdatarct_macos.a \
   -headers ./Sources/ \
-  -library ./Output/libdatarct_ios.a \
+  -library ./.out/libdatarct_ios.a \
   -headers ./Sources/ \
-  -library ./Output/libdatarct_ios_simulator.a \
+  -library ./.out/libdatarct_ios_simulator.a \
   -headers ./Sources/ \
-  -output Output/DataRCT.xcframework
+  -output .out/DataRCT.xcframework
 
-zip -r Output/DataRCT-Apple.zip Output/DataRCT.xcframework
+zip -r DataRCT.xcframework.zip .out/DataRCT.xcframework
 
-rm -rf Output/libdatarct_ios_simulator.a
-rm -rf Output/libdatarct_ios.a
-rm -rf Output/libdatarct_macos.a
-rm -rf Output/DataRCT.xcframework
+rm -rf .out
 
 echo "Done."
